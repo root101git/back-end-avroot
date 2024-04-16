e1cba60c9a6c850658686e5147a55c576f3ef88f
Rohan <rohanb158@gmail.com>
2024-04-16 11:43:56 +0530

    pre final commit

--- routes/searching.py
+++ routes/searching.py
@@ -10,6 +10,7 @@ db = firestore.client()
 @router.get('/search/{input}')
 async def index(input:str=""):
     doc_ref = db.collection("data_plants")
-    data = doc_ref.where("name", ">=", input).where("name", "<=",input+ "\uf8ff").get()
+    data = doc_ref.where("plant_name", ">=", input).where("plant_name", "<=",input+ "\uf8ff").stream()
+    print(data)
     result =  [elem.to_dict() for elem in data]
     return result
